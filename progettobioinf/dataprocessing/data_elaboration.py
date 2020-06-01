8fe6a382f76f9946dd39efa55d416f8b2aedcb7c
Salvatore <salvatore.vestita.1993@gmail.com>
2020-06-01 13:57:52 +0200

    Update epigenomic data elaboration and add general test

--- progettobioinf/dataprocessing/data_elaboration.py
+++ progettobioinf/dataprocessing/data_elaboration.py
@@ -2,7 +2,13 @@ from sklearn.impute import KNNImputer
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import RobustScaler
-from pathlib import Path
+from tqdm.auto import tqdm # A simple loading bar
+from scipy.stats import entropy
+from scipy.stats import pearsonr
+from scipy.stats import spearmanr
+from minepy import MINE
+import numpy as np
+import seaborn as sns
 
 # Rate between features and samples
 def rate_features_samples(epigenomes):
@@ -43,8 +49,7 @@ def check_class_balance(labels):
     for axis, (region, y) in zip(axes.ravel(), labels.items()):
         y.hist(ax=axis, bins=3)
         axis.set_title(f"Classes count in {region}")
-    fig.savefig('class_balance.png')
-    # TODO cambia il modo di salvataggio inserendo l'immagine in una cartella img
+    fig.savefig('img/class_balance.png')
 
 # Constant Features
 def __drop_const_features(df:pd.DataFrame)->pd.DataFrame:
@@ -74,17 +79,85 @@ def data_normalization(epigenomes):
         for region, x in epigenomes.items()
     }
 
-## TODO continua da questo punto
-
 # Correlation with output
 
 ## Linear Correlation (Pearson)
+def execute_pearson(epigenomes, labels, p_value_threshold, uncorrelated):
+    for region, x in epigenomes.items():
+        for column in tqdm(x.columns, desc=f"Running Pearson test for {region}", dynamic_ncols=True, leave=False):
+            correlation, p_value = pearsonr(x[column].values.ravel(), labels[region].values.ravel())
+            if p_value > p_value_threshold:
+                print(region, column, correlation)
+                uncorrelated[region].add(column)
 
 ## Monotonic Correlation (Spearman)
+def execute_spearman(epigenomes, labels, p_value_threshold, uncorrelated):
+    for region, x in epigenomes.items():
+        for column in tqdm(x.columns, desc=f"Running Spearman test for {region}", dynamic_ncols=True, leave=False):
+            correlation, p_value = spearmanr(x[column].values.ravel(), labels[region].values.ravel())
+            if p_value > p_value_threshold:
+                print(region, column, correlation)
+                uncorrelated[region].add(column)
 
 ## Non-Linear Correlation (MIC)
+def execute_mic(epigenomes, labels, correlation_threshold, uncorrelated):
+    for region, x in epigenomes.items():
+        for column in tqdm(uncorrelated[region], desc=f"Running MINE test for {region}", dynamic_ncols=True,
+                           leave=False):
+            mine = MINE()
+            mine.compute_score(x[column].values.ravel(), labels[region].values.ravel())
+            score = mine.mic()
+            if score < correlation_threshold:
+                print(region, column, score)
+            else:
+                uncorrelated[region].remove(column)
 
 ### Drop features uncorrelated with output
+def drop_features(epigenomes, uncorrelated):
+    for region, x in epigenomes.items():
+        epigenomes[region] = x.drop(columns=[
+            col
+            for col in uncorrelated[region]
+            if col in x.columns
+        ])
 
-# Correlazione Features
+# Features correlations
+def check_features_correlations(epigenomes, scores, p_value_threshold, correlation_threshold, extremely_correlated):
+    for region, x in epigenomes.items():
+        for i, column in tqdm(
+                enumerate(x.columns),
+                total=len(x.columns), desc=f"Running Pearson test for {region}", dynamic_ncols=True, leave=False):
+            for feature in x.columns[i + 1:]:
+                correlation, p_value = pearsonr(x[column].values.ravel(), x[feature].values.ravel())
+                correlation = np.abs(correlation)
+                scores[region].append((correlation, column, feature))
+                if p_value < p_value_threshold and correlation > correlation_threshold:
+                    print(region, column, feature, correlation)
+                    if entropy(x[column]) > entropy(x[feature]):
+                        extremely_correlated[region].add(feature)
+                    else:
+                        extremely_correlated[region].add(column)
 
+# Most "n" correlated touples
+def detect_most_n_correlated_touples(epigenomes, scores, n, labels):
+    for region, x in epigenomes.items():
+        _, firsts, seconds = list(zip(*scores[region][:n]))
+        columns = list(set(firsts + seconds))
+        print(f"Most correlated features from {region} epigenomes")
+        sns.pairplot(pd.concat([
+            x[columns],
+            labels[region],
+        ], axis=1), hue=labels[region].columns[0])
+        plt.savefig('img/scatter_plot_correlated_'+region+".png")
+
+# Most "n" uncorrelated touples
+def detect_most_n_uncorrelated_touples(epigenomes, scores, n, labels):
+    for region, x in epigenomes.items():
+        _, firsts, seconds = list(zip(*scores[region][-n:]))
+        columns = list(set(firsts + seconds))
+        print(f"Least correlated features from {region} epigenomes")
+        sns.pairplot(pd.concat([
+            x[columns],
+            labels[region],
+        ], axis=1), hue=labels[region].columns[0])
+        plt.savefig('img/scatter_plot_uncorrelated_'+region+".png")
--- progettobioinf/dataprocessing/data_processing.py
+++ progettobioinf/dataprocessing/data_processing.py
@@ -3,18 +3,25 @@ from ucsc_genomes_downloader import Genome
 
 from progettobioinf.dataprocessing.data_retrieval import *
 from progettobioinf.dataprocessing.data_elaboration import *
+from progettobioinf.dataprocessing.utility import *
 
 cell_line = "K562"
 assembly = "hg19"
 window_size = 200
 
+# Initial Setup
 # Step 1. Data Retrieval
 # Step 2. Data Elaboration
 # Step 3. Data Visualization
 
 if __name__ == '__main__':
 
+    # Initial Setup
+    print('Initial Setup...')
+    create_img_folder()
+
     # Step 1. Data Retrieval
+    print('Step 1. Data Retrieval')
 
     ## Epigenomic
     promoters_epigenomes, promoters_labels = load_epigenomes(
@@ -65,6 +72,7 @@ if __name__ == '__main__':
     print(sequences["enhancers"][:2])
 
     # Step 2. Data elaboration
+    print('Step 2. Data elaboration')
 
     ## Rate between features and samples
     rate_features_samples(epigenomes)
@@ -83,3 +91,50 @@ if __name__ == '__main__':
 
     ## Z-Scoring
     data_normalization(epigenomes)
+
+    p_value_threshold = 0.01
+    correlation_threshold = 0.05
+
+    uncorrelated = {
+        region: set()
+        for region in epigenomes
+    }
+
+    ## Pearson
+    execute_pearson(epigenomes, labels, p_value_threshold, uncorrelated)
+
+    ## Spearman
+    execute_spearman(epigenomes, labels, p_value_threshold, uncorrelated)
+
+    ## MIC
+    execute_mic(epigenomes, labels, correlation_threshold, uncorrelated)
+
+    ## Drop features uncorrelated with output
+    drop_features(epigenomes, uncorrelated)
+
+    extremely_correlated = {
+        region: set()
+        for region in epigenomes
+    }
+
+    scores = {
+        region: []
+        for region in epigenomes
+    }
+
+    # Features correlations
+    check_features_correlations(epigenomes, scores, p_value_threshold, correlation_threshold, extremely_correlated)
+
+    # Sort the obtained scores
+    scores = {
+        region: sorted(score, key=lambda x: np.abs(x[0]), reverse=True)
+        for region, score in scores.items()
+    }
+
+    ## Scatter plot (most correlated touples)
+    detect_most_n_correlated_touples(epigenomes, scores, 3, labels)
+
+    ## Scatter plot (most uncorrelated touples)
+    detect_most_n_uncorrelated_touples(epigenomes, scores, 3, labels)
+
+    # TODO Features distributions
