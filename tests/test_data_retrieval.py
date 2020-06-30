defc91bac81cee79c516c378a35ebf03bd3b3ce8
Salvatore <salvatore.vestita.1993@gmail.com>
2020-06-30 16:44:59 +0200

    Add data retrieval test

---
+++ tests/test_data_retrieval.py
@@ -0,0 +1,35 @@
+from progettobioinf.data_retrieval import *
+
+
+def test_retrieve_epigenomes_labels():
+    retrieve_epigenomes_labels("K562", 200)
+
+
+def test_retrieve_sequences():
+    genome = get_genome("hg19")
+    epi, lab = retrieve_epigenomes_labels("MCF-7", 200)
+    seq = retrieve_sequences(epi, genome, 200)
+
+
+def test_are_sequences_retrieved():
+    are_sequences_retrieved("K562")
+
+
+def test_are_epigenomics_retrieved():
+    are_epigenomics_retrieved("K562")
+
+
+def test_are_labels_retrieved():
+    are_labels_retrieved("K562")
+
+
+def test_are_epigenomics_elaborated():
+    are_epigenomics_elaborated("K562")
+
+
+def test_are_data_visualized():
+    are_data_visualized("K562")
+
+
+def test_are_data_retrieved():
+    are_data_retrieved("K562")
---
+++ tests/test_initial.py
@@ -0,0 +1,21 @@
+from progettobioinf.data_retrieval import *
+
+
+def test_init():
+    initial_setup("K562")
+
+
+def test_init_already_exists():
+    initial_setup("K562")
+
+
+def test_data_retrieval():
+    are_data_retrieved("K562")
+
+
+def test_data_visualized():
+    are_data_visualized("K562")
+
+
+def test_data_elaborated():
+    are_data_elaborated("K562", 3, 5)
