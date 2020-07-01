7a4975bad2673f4d6fddf7f56409cd811554948d
Salvatore <salvatore.vestita.1993@gmail.com>
2020-07-01 11:02:12 +0200

    Update test

--- tests/test_data_retrieval.py
+++ tests/test_data_retrieval.py
@@ -1,13 +1,9 @@
 from progettobioinf.data_retrieval import *
-from progettobioinf.data_processing import *
 
 def test_retrieve():
     genome = get_genome("hg19")
     epi, lab = retrieve_epigenomes_labels("K562", 200)
     seq = retrieve_sequences(epi, genome, 200)
-
-    data_retrieval("K562", genome, 200)
-
 
 def test_are_sequences_retrieved():
     are_sequences_retrieved("K562")
